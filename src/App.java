0c92db4a14e705c2c6d232e19cb81745540dd34e
Jonatan David Rodriguez <inge.jonatanrodriguez@gmail.com>
2024-11-20 22:16:04 -0500

    creacion de clase

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ahora si es hora de trabajar!!");
-        System.out.println("Esto es una version");
-        
+        var scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
